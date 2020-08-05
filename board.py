d84dfeecd3da19a7e10b01c2db3e897c003ac4f4
TehJazz22 <69231410+TehJazz22@users.noreply.github.com>
2020-08-05 16:53:05 -0700

    Update board.py

--- board.py
+++ board.py
@@ -1,10 +1,12 @@
+#The following class represents the board of the game
 class Board:
+    #Initializes the properties of the board
     def __init__(self):
         self.board = {
             "TL": " ", "TM": " ", "TR": " ",
             "ML": " ", "MM": " ", "MR": " ",
             "BL": " ", "BM": " ", "BR": " "}
-
+    #Draws the board
     def print_board(self):
         print(self.board["TL"] + "|" + self.board["TM"] \
               + "|" + self.board["TR"] + "|")
@@ -14,18 +16,18 @@ class Board:
         print("-+" * 3)
         print(self.board["BL"] + "|" + self.board["BM"] \
               + "|" + self.board["BR"] + "|")
-
+    #Checks for invalid moves
     def _is_valid_move(self, position):
         if self.board[position] == " ":
             return True
         return False
-
+    #Updates the board
     def change_board(self, position, type):
         if self._is_valid_move(position):
             self.board[position] = type
             return self.board
         return None
-
+    #Checks for winning conditions
     def is_winner(self, player):
         if self.board["TL"] == player.type and self.board["TM"] == player.type and self.board["TR"] == player.type or \
                 self.board["ML"] == player.type and self.board["MM"] == player.type and self.board[
